76fe26e7a6c9962008a5bf9f9bfeeb85aef32b86
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-24 17:16:50 -0300

    Alterando o visual conforme o dado presente

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,13 +13,19 @@ var people = [
     name: 'Karina do Amaral',
     tel: '14999999999',
     xp: false
+  },
+  {
+    name: 'Cauê Sanches Padula',
+    tel: '14999999999',
+    xp: true
   }
 ]
 
-console.log(document.querySelector('table.lista tbody').innerHTML);
 
 for (person in people){
-  document.querySelector('table.lista tbody').innerHTML += `<tr>
+  console.log(person)
+  document.querySelector('table.lista tbody').innerHTML += `
+  <tr style = "background-color: ${ ((person % 2 == 0) ? '#fff' : '#eee')}">
     <td>
       ${ people[person].name }
     </td>
@@ -27,10 +33,11 @@ for (person in people){
       ${ people[person].tel }
     </td>
     <td>
-      ${ (people[person].xp ? 'Sim' : 'Não') }
+      ${ (people[person].xp ? '<strong style = "color:green">Sim</strong>' : '<strong style = "color:red">Não</strong>') }
     </td>
     <td>
       <button>Alterar</button>
     </td>
-  </tr>`
+  </tr>
+  `
 }
