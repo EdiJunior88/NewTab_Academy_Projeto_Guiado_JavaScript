5884981d85776969f5cb203025feef941872c13f
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-24 16:53:14 -0300

    Listando os dados na página

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,36 @@
-console.log('Oi, eu sou o JavaScript') 
-console.log('tudo bem, como vai?')
+var people = [
+  {
+    name: 'Fernando Henrique Caversan Santos Duro',
+    tel: '14999999999',
+    xp: true
+  },
+  {
+    name: 'Anderson Arcenio Matos da Costa',
+    tel: '14999999999',
+    xp: true
+  },
+  {
+    name: 'Karina do Amaral',
+    tel: '14999999999',
+    xp: false
+  }
+]
+
+console.log(document.querySelector('table.lista tbody').innerHTML);
+
+for (person in people){
+  document.querySelector('table.lista tbody').innerHTML += `<tr>
+    <td>
+      ${ people[person].name }
+    </td>
+    <td>
+      ${ people[person].tel }
+    </td>
+    <td>
+      ${ (people[person].xp ? 'Sim' : 'Não') }
+    </td>
+    <td>
+      <button>Alterar</button>
+    </td>
+  </tr>`
+}
