b98326a96b1f6fc11d8c4939da37c5310d1d3c53
EdiJunior88 <er.moura88@yahoo.com.br>
2022-02-01 21:45:27 -0300

    Inclusão de item na LocalStorage pt 1

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,19 @@
 function testaFormulario(e){
   e.preventDefault();
 
-  console.log('oi');
+  var peopleRaw = localStorage.getItem('people');
+  if (peopleRaw != null) {
+    var people = JSON.parse(peopleRaw);
+  }
+  else {
+    var people = [];
+  }
+
+  /* people.push({
+    name: e.target.elements['name'].value,
+    tel: e.target.elements['name'].value,
+    xp: ,
+  }); */
+
+  console.log(e.target.elements['name'].value);
 }
